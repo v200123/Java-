0f8a45d72bcf568495262cd2ed8c840107d1a765
LC <295799682@qq.com>
2018-03-23 09:15:07 +0800

    写一个记事本

---
+++ eclipse-workspace/notebook/src/notebook/NoteBook.java
@@ -0,0 +1,30 @@
+package notebook;
+
+public class NoteBook {
+	
+	public void add(String s) {
+		
+	}
+	
+	public int gteSize() {
+		return 0;
+	}
+	
+	public String getNote(int index) {
+		return "";
+	}
+	
+	public boolean removeNote(int index) {
+		return true;
+	}
+	
+	public String[] list() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
